87f9fba015dc3d87d24fd12e9c559b38cffd9bde
isarmendes <123502060+isarmendes@users.noreply.github.com>
2024-03-13 16:39:24 -0300

    Initial commit

---
+++ src/screens/Profile/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import styles from './styles'
+import Title from '../../components/Title'
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <Title title='Diva babilonica que não tem braço🎂🎉'/>
+    </View>
+  )
+}
